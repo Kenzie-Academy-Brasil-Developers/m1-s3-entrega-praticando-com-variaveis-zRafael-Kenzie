5e5ccba5ef28cabd996b1d0a0679ba410a57ac01
zRafael-Kenzie <106535314+zRafael-Kenzie@users.noreply.github.com>
2022-06-14 09:50:44 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Rafael"
+let sobrenome = "Soares"
+let anoNascimento = 2001
+let cidade = "Bezerros"
+
+console.log ("Meu nome é " + nome + " " + sobrenome + " tenho " + (2022 - anoNascimento) + " anos e moro em " + cidade)
